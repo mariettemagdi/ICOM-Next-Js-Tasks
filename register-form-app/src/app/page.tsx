3747828bbd5a891c97aba72f70b431fcc59e0f7a
mariettemagdi <mariettemagdi@yahoo.com>
2025-12-11 00:55:09 +0200

    install dependencies and required components from shadcn

---
+++ register-form-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import RegisterForm from "@/components/RegisterForm";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <main>
+    <RegisterForm />
+   </main>
+  );
+}
---
+++ register-form-app/src/components/RegisterForm.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from 'react'
+
+export default function RegisterForm() {
+  return (
+    <div>RegisterForm</div>
+  )
+}
